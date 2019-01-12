ccd3ae9372ab7b9440e3133fd74abf5169cdd8c6
Karan Agarwal <karan@Karans-MacBook-Pro.local>
2019-01-12 21:20:53 +0530

    Add functionality to update the user account information

--- blogapp/routes.py
+++ blogapp/routes.py
@@ -1,4 +1,8 @@
-from blogapp.forms import RegistrationForm, LoginForm
+
+import os
+import secrets
+from PIL import Image
+from blogapp.forms import RegistrationForm, LoginForm, UserAccountForm
 from flask import render_template, url_for, flash, redirect,request
 from blogapp import app,db,bcrypt
 from blogapp.models import User, Post
@@ -66,8 +70,37 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(picture):
+    random_hex = secrets.token_hex(8)
+    _ , f_ext = os.path.splitext(picture.filename)
+    f_name = random_hex + f_ext
+
+    saving_path = os.path.join(app.root_path, 'static/profile_pics',f_name)
+
+    output_size = (125,125)
+    i=Image.open(picture)
+    i.thumbnail(output_size)
+    i.save(saving_path)
+
+    return f_name
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UserAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            f_name = save_picture(form.picture.data)
+            current_user.image_file=f_name
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!','success')
+        return redirect(url_for('account'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static',filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html',title='Account',image_file = image_file)
+    return render_template('account.html',title='Account',image_file = image_file,
+                            form=form)
